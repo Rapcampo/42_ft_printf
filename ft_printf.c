1663e4b5b602792cd8adffff09d6026c442a7dd9
Rapcampo <raphael.campospt@gmail.com>
2023-10-24 11:38:46 +0100

    structure of printf is mostly done

--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,25 @@
-int ft_printf(const char *, ...);
+#include "ft_printf.h"
+
+int ft_printf(const char *format, ...);
+
+int ft_printf(const char *format, ...)
+{
+    auto int i;
+    va_list args; 
+    va_start(args, format);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            base_check(format, args);
+        }
+        else 
+        {
+            print_str(format, args);
+        }
+        i++;
+    va_end(args);
+    return (format);
+}
+
---
+++ ft_printf.h
@@ -0,0 +1,15 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+//libraries to be used
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+// functions used
+
+int	 ft_printf(const char *format, ...);
+
+#endif
